05981a9aa356eca3b7af5ba60ca01fe96577ee86
Mtho <mthontanzi99@gmail.com>
2023-08-04 08:24:00 +0200

    Add Tailwind and responsiveness

--- ecommerce-product-page-main/src/Navbar.js
+++ ecommerce-product-page-main/src/Navbar.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import { ReactComponent as Logo } from './images/logo.svg';
 import { ReactComponent as Cart } from './images/icon-cart.svg';
+import { ReactComponent as Menu } from './images/icon-menu.svg';
+
 import avatar from './images/image-avatar.png'
 import './Navbar.css'
 
 export default function Navbar() {
   return (
-      <div className='Navbar flex justify-between h-20'>
+      <div className='Navbar flex justify-between items-center h-20 px-2'>
+      
+      <div className='flex gap-4'>
+        <div className='menuBar md:hidden'>
+          <Menu />
+        </div>
         <Logo />
         <div className='menu hidden md:inline-block'>
-            <a href='/'>Collections</a>
-            <a href='/'>Men</a>
-            <a href='/'>Women</a>
-            <a href='/'>About</a>
-            <a href='/'>Contact</a>
-        </div>
+            <a href='/' className='px-3'>Collections</a>
+            <a href='/' className='px-3'>Men</a>
+            <a href='/' className='px-3'>Women</a>
+            <a href='/' className='px-3'>About</a>
+            <a href='/' className='px-3'>Contact</a>
+        </div>      
+      </div>
+        
         <div className='flex'>
             < Cart />
-            <img src={ avatar } alt='avatar image' />       
+            <img src={ avatar } alt='avatar' className='h-12'/>       
         </div>
     </div>
   )
